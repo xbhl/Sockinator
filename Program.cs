067d3988d422d1d9a364301f0179b71b5215899f
xi <77388034+xbhl@users.noreply.github.com>
2023-12-24 22:19:00 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace sockettest
+{
+    class Sockinator
+    {
+        Dictionary<string, string> server_config = new Dictionary<string, string>()
+        {
+            { "ip", "IP/DNS GOES HERE" },
+            { "port", "PORT GOES HERE" },
+        };
+
+        private Socket socket;
+
+        public void Connect()
+        {
+            try
+            {
+                IPAddress addr = GetAddress();
+
+                IPEndPoint ep = new IPEndPoint(addr, int.Parse(server_config["port"]));
+                Console.WriteLine("Created endpoint");
+
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Console.WriteLine("Created socket");
+
+                socket.BeginConnect(ep, ConnectionCallback, socket);
+                Console.WriteLine("Connected to server");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private IPAddress GetAddress()
+        {
+                IPAddress[] addr = Dns.GetHostAddresses(server_config["ip"]);
+                return addr[0];
+        }
+
+        private string getMessage()
+        {
+            Console.Write("Payload: ");
+            return Console.ReadLine();
+        }
+
+        private void ConnectionCallback(IAsyncResult ar)
+        {
+            try
+            {
+                socket.EndConnect(ar);
+
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(getMessage());
+                socket.Send(data);
+                Console.WriteLine("Sent message");
+                byte[] buffer = new byte[1024];
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, socket);
+                Console.WriteLine("Receiving data...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket receivedSocket = (Socket)ar.AsyncState;
+
+                int bytesRead = receivedSocket.EndReceive(ar);
+
+                byte[] buffer = new byte[1024];
+                receivedSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, receivedSocket);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sockinator sock = new Sockinator();
+            sock.Connect();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+This is a learning project.
+You can use this if you find it useful.
+
+It connects the client to a socket and you can extend this.
+As this is a beginner project, any PR's and suggestions are appreciated.
